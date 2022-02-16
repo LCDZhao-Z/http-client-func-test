53136998e8291e6b5128ff16fe3b16a71dc4da80
LCDZhao-O <weizhao@oppo.com>
2022-02-16 15:04:32 +0800

    complete

--- client/src/main/java/io/esastack/test/func/http/InterceptTest.java
+++ client/src/main/java/io/esastack/test/func/http/InterceptTest.java
@@ -6,7 +6,6 @@ import io.esastack.test.func.ClientUtils;
 
 import java.io.File;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.CompletionStage;
 
 public class InterceptTest {
 
@@ -15,21 +14,24 @@ public class InterceptTest {
 
 
     public static void main(String[] args) throws Exception {
+        testPostWithoutBody();
+        testContinue100();
         testRetry();
+        testReadTimeout();
     }
 
-//    private static void testPostWithoutBody() throws Exception {
-//        HttpResponse response = ClientUtils.client()
-//                .post("http://localhost:8080/rest/" + POST_WITHOUT_BODY)
-//                .execute()
-//                .get();
-//        if (response.status() == 200) {
-//            System.out.println("Success");
-//        } else {
-//            System.err.println("Error occur!status:" + response.status() +
-//                    ", body:" + response.body().string(StandardCharsets.UTF_8));
-//        }
-//    }
+    private static void testPostWithoutBody() throws Exception {
+        HttpResponse response = ClientUtils.client()
+                .post("http://localhost:8080/rest/" + POST_WITHOUT_BODY)
+                .execute()
+                .get();
+        if (response.status() == 200) {
+            System.out.println("Success");
+        } else {
+            System.err.println("Error occur!status:" + response.status() +
+                    ", body:" + response.body().string(StandardCharsets.UTF_8));
+        }
+    }
 
     private static void testContinue100() throws Exception {
         HttpResponse response = ClientUtils.client()
@@ -46,21 +48,21 @@ public class InterceptTest {
                     ", body:" + response.body().string(StandardCharsets.UTF_8));
         }
 
-//        RestResponseBase responseBase = ClientUtils.rest()
-//                .post(URL_PREFIX + "100-continue")
-//                .entity(new File("D://1GB"))
-//                .maxRetries(0)
-//                .readTimeout(Integer.MAX_VALUE)
-//                .execute()
-//                .toCompletableFuture()
-//                .get();
-//
-//        if (responseBase.status() == 200) {
-//            System.out.println("Success");
-//        } else {
-//            System.err.println("Error occur!status:" + responseBase.status() +
-//                    ", body:" + responseBase.bodyToEntity(String.class));
-//        }
+        RestResponseBase responseBase = ClientUtils.rest()
+                .post(URL_PREFIX + "100-continue")
+                .entity(new File("D://1GB"))
+                .maxRetries(0)
+                .readTimeout(Integer.MAX_VALUE)
+                .execute()
+                .toCompletableFuture()
+                .get();
+
+        if (responseBase.status() == 200) {
+            System.out.println("Success");
+        } else {
+            System.err.println("Error occur!status:" + responseBase.status() +
+                    ", body:" + responseBase.bodyToEntity(String.class));
+        }
     }
 
     private static void testReadTimeout() throws Exception {
@@ -77,35 +79,34 @@ public class InterceptTest {
                     ", body:" + response.body().string(StandardCharsets.UTF_8));
         }
 
+        RestResponseBase restResponse = ClientUtils.rest()
+                .post(URL_PREFIX + "readTimeout")
+                .readTimeout(2000)
+                .execute()
+                .toCompletableFuture()
+                .get();
 
-//        RestResponseBase restResponse = ClientUtils.rest()
-//                .post(URL_PREFIX + "readTimeout")
-//                .readTimeout(2000)
-//                .execute()
-//                .toCompletableFuture()
-//                .get();
-//
-//        if (restResponse.status() == 200) {
-//            System.out.println("Success");
-//        } else {
-//            System.err.println("Error occur!status:" + restResponse.status() +
-//                    ", body:" + restResponse.bodyToEntity(String.class));
-//        }
+        if (restResponse.status() == 200) {
+            System.out.println("Success");
+        } else {
+            System.err.println("Error occur!status:" + restResponse.status() +
+                    ", body:" + restResponse.bodyToEntity(String.class));
+        }
     }
 
     private static void testRetry() throws Exception {
-//        HttpResponse response = ClientUtils.client()
-//                .post(URL_PREFIX + "retry")
-//                .maxRetries(5)
-//                .execute()
-//                .get();
-//
-//        if (response.status() == 200) {
-//            System.out.println("Success");
-//        } else {
-//            System.err.println("Error occur!status:" + response.status() +
-//                    ", body:" + response.body().string(StandardCharsets.UTF_8));
-//        }
+        HttpResponse response = ClientUtils.client()
+                .post(URL_PREFIX + "retry")
+                .maxRetries(5)
+                .execute()
+                .get();
+
+        if (response.status() == 200) {
+            System.out.println("Success");
+        } else {
+            System.err.println("Error occur!status:" + response.status() +
+                    ", body:" + response.body().string(StandardCharsets.UTF_8));
+        }
 
         RestResponseBase restResponse = ClientUtils.rest()
                 .post(URL_PREFIX + "retry")
@@ -120,36 +121,5 @@ public class InterceptTest {
             System.err.println("Error occur!status:" + restResponse.status() +
                     ", body:" + restResponse.bodyToEntity(String.class));
         }
-    }
-
-    private static void testInterceptorAndFilter() throws Exception {
-        HttpResponse response = ClientUtils.client()
-                .post(URL_PREFIX + "interceptorAndFilter")
-                .body("aaaaa".getBytes())
-                .execute()
-                .get();
-
-        if (response.status() == 200) {
-            System.out.println("Success!" + response.version());
-        } else {
-            System.err.println("Error occur!status:" + response.status() +
-                    ", body:" + response.body().string(StandardCharsets.UTF_8));
-        }
-
-        /*RestResponseBase restResponse = ClientUtils.rest()
-                .post(URL_PREFIX + "interceptorAndFilter")
-                .entity("aaaaa".getBytes())
-                .readTimeout(Integer.MAX_VALUE)
-                .disableExpectContinue()
-                .execute()
-                .toCompletableFuture()
-                .get();
-
-        if (restResponse.status() == 200) {
-            System.out.println("Success!" + restResponse.version());
-        } else {
-            System.err.println("Error occur!status:" + restResponse.status() +
-                    ", body:" + restResponse.bodyToEntity(String.class));
-        }*/
     }
 }
